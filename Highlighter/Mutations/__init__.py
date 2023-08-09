e9faec591bef5a0d4cbb6a7ecc645247ff177448
PeterDarley <52110396+PeterDarley@users.noreply.github.com>
2023-08-09 11:56:10 -0700

    Starting monkey patching MultipleSeqAlignment

--- Highlighter/Mutations/__init__.py
+++ Highlighter/Mutations/__init__.py
@@ -1,32 +1,64 @@
 from Bio import Align
 from Bio.Seq import Seq
 from Bio.SeqRecord import SeqRecord
+from Bio.Align import MultipleSeqAlignment
 
-
-class Mutations(Align.MultipleSeqAlignment):
-    """ A class to store mutations in a multiple sequence alignment """
-
-
-def get_mutations(*, sequence, reference):
-    """ Get mutations from a list of sequences and a reference sequence """
+def get_mutations(*, sequence: str|Seq|SeqRecord, reference: str|Seq|SeqRecord, apobec: bool=False, g_to_a: bool=False, seq_type: str="NT") -> dict[int: list]:
+    """ Get mutations from a list of sequences and a reference sequence 
+    returns a dictionary of mutations where the key is the position of the mutation and the value is a list of types of mutations """
 
     if type(sequence) is Seq:
             sequence = str(sequence)
     elif type(sequence) is  SeqRecord:
             sequence = str(sequence.seq)
     elif type(sequence) is not str:
-            raise TypeError(f'Expected sequence to be a string, Seq, or SeqRecord, got {type(sequence)}')
+            raise TypeError(f"Expected sequence to be a string, Seq, or SeqRecord, got {type(sequence)}")
 
     if type(reference) is Seq:
             reference = str(reference)
     elif type(sequence) is  SeqRecord:
             reference = str(reference.seq)
     elif type(reference) is not str:
-            raise TypeError(f'Expected reference to be a string, Seq, or SeqRecord, got {type(reference)}')
+            raise TypeError(f"Expected reference to be a string, Seq, or SeqRecord, got {type(reference)}")
         
     if len(sequence) != len(reference):
-        raise ValueError('Reference and sequence must be the same length')
+        raise ValueError("Reference and sequence must be the same length")
     
-    # for base_index in range(0, len(sequence)-1):
-    #     if base_
-        
+    mutations: dict = {}
+
+    if sequence == reference:
+        return mutations
+
+    for base_index in range(len(sequence)):
+        if reference[base_index] != sequence[base_index]:
+            mutations[base_index] = []
+            
+            if sequence[base_index] != "-":
+                mutations[base_index].append(sequence[base_index])
+            else:
+                mutations[base_index].append("Gap")
+
+            if seq_type=="NT":
+                if reference[base_index] == "G" and sequence[base_index] == "A":
+                    if g_to_a:
+                        mutations[base_index].append("G->A mutation")
+
+                    if apobec and base_index < len(sequence)-3 and sequence[base_index+1] in "AG" and sequence[base_index+2] != "C":
+                        mutations[base_index].append("APOBEC")
+    
+    return mutations
+
+
+def monkey_patch_align_mutations():
+    """ Monkey patch Bio.Align.MultipleSeqAlignment with Mutations """
+
+    def get_mutations(self, type: str="NT", apobec: bool=False) -> dict[int: list]:
+        """ Get mutations from a MultipleSeqAlignment object 
+        returns a dictionary of mutations where the key is the position of the mutation and the value is a list of types of mutations """
+
+        return "test"
+    
+    MultipleSeqAlignment.get_mutations = get_mutations
+
+# Do the monkey patching
+monkey_patch_align_mutations()
--- Highlighter/Tests/test_mutations.py
+++ Highlighter/Tests/test_mutations.py
@@ -1,24 +1,14 @@
 import unittest
-from Bio import Seq, SeqRecord
-from Highlighter.Mutations import Mutations, get_mutations
+import pathlib
+
+from Bio import AlignIO
+from Bio.Seq import Seq
+from Bio.SeqRecord import SeqRecord
+
+from Highlighter.Mutations import get_mutations
 
 class MutationTests(unittest.TestCase):
     """ Test the mutation object """
-
-    def test_mutations_start_out_empty(self):
-        """ Mutations should start out empty """
-
-        mutations = Mutations([])
-
-        self.assertEqual(len(mutations), 0)
-
-    def test_mutations_sequences_can_be_added(self):
-        """ Mutations should be able to add sequences """
-
-        mutations = Mutations([])
-        mutations.append(SeqRecord.SeqRecord(Seq.Seq('ATGC')))
-
-        self.assertEqual(len(mutations), 1)
 
     def test_get_mutations_error_if_no_sequence(self):
         """ get_mutations should error if no sequence is provided """
@@ -31,6 +21,64 @@ class MutationTests(unittest.TestCase):
 
         with self.assertRaises(TypeError):
             get_mutations(sequence='ATGC')
+
+    def test_get_mutations_error_if_bad_types(self):
+        """ get_mutations should error if bad types are provided """
+
+        with self.assertRaises(TypeError):
+            get_mutations(sequence=1, reference='ATGC')
+
+        with self.assertRaises(TypeError):
+            get_mutations(sequence='ATGC', reference=[])
+
+    def test_get_mutations_error_if_different_lengths(self):
+        """ get_mutations should error if the sequence and reference are different lengths """
+
+        with self.assertRaises(ValueError):
+            get_mutations(sequence='ATGC', reference='ATG')
+
+    def test_get_mutations_succeeds_with_mixed_types(self):
+        """ get_mutations should succeed with mixed types """
+
+        try:
+            get_mutations(sequence='ATGC', reference=Seq('ATGC'))
+        except:
+            self.fail('get_mutations failed with mixed types')
+
+        try:
+            get_mutations(sequence=SeqRecord(Seq('ATGC')), reference='ATGC')
+        except:
+            self.fail('get_mutations failed with mixed types')
+
+    def test_get_mutations_returns_dict(self):
+        """ get_mutations should return a dictionary """
+
+        self.assertEqual(type(get_mutations(sequence='ATGC', reference='ATGC')), dict)
+
+    def test_get_mutations_returns_empty_dict_given_same_sequence_and_reference(self):
+        """ get_mutations should return an empty dictionary if the sequence and reference are the same """
+
+        self.assertEqual(get_mutations(sequence='ATGC', reference='ATGC'), {})
+
+    def test_get_mutations_returns_non_empty_dict_given_different_sequence_and_reference(self):
+        """ get_mutations should return a non-empty dictionary if the sequence and reference are different """
+
+        self.assertNotEqual(get_mutations(sequence='ATGC', reference='ATGG'), {})
+
+    def test_get_mutations_returns_correct_dict_given_different_sequence_and_reference(self):
+        """ get_mutations should return a dictionary with the correct keys and values """
+
+        self.assertEqual(get_mutations(reference='GTGCGGC-', sequence='AATGCA-T'), {0: ['A'], 1: ['A'], 2: ['T'], 3: ['G'], 4: ['C'], 5: ['A'], 6: ['Gap'], 7: ['T']})
+        self.assertEqual(get_mutations(g_to_a=True, reference='GTGCGGC-', sequence='AATGCA-T'), {0: ['A', 'G->A mutation'], 1: ['A'], 2: ['T'], 3: ['G'], 4: ['C'], 5: ['A', 'G->A mutation'], 6: ['Gap'], 7: ['T']})
+        self.assertEqual(get_mutations(apobec=True, reference='GTGCGGC-', sequence='AATGCA-T'), {0: ['A', 'APOBEC'], 1: ['A'], 2: ['T'], 3: ['G'], 4: ['C'], 5: ['A'], 6: ['Gap'], 7: ['T']})
+        self.assertEqual(get_mutations(g_to_a=True, apobec=True, reference='GTGCGGC-', sequence='AATGCA-T'), {0: ['A', 'G->A mutation', 'APOBEC'], 1: ['A'], 2: ['T'], 3: ['G'], 4: ['C'], 5: ['A', 'G->A mutation'], 6: ['Gap'], 7: ['T']})
+
+    def test_stuff_with_bio_align(self):
+        """ Test stuff with Bio.Align """
+
+        align = AlignIO.read(pathlib.PurePath(pathlib.Path(__file__).parent.resolve(), 'Mutations/test.fasta'), 'fasta')
+
+        self.assertEqual(align.get_mutations(), "test")
 
 if __name__ == '__main__':
     unittest.main()
