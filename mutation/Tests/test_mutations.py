c6bbfe7f1c82f2d9ff333244b1bf97c4e0ecdabd
PeterDarley <52110396+PeterDarley@users.noreply.github.com>
2023-09-11 14:33:06 -0700

    Finished tests for actual file creation

--- mutation/Tests/test_mutations.py
+++ mutation/Tests/test_mutations.py
@@ -1,5 +1,8 @@
 import unittest, os, pathlib
 
+from itertools import product
+from pprint import pprint
+
 import mutations
 
 from Bio import AlignIO, Phylo, SeqUtils
@@ -232,6 +235,13 @@ class MutationPlotTests(unittest.TestCase):
         self.tree_hiv_nt = Phylo.read('mutation/Tests/Mutation/hiv_nt_nexus.tre', 'nexus')
         self.mutation_plot_hiv_nt = MutationPlot(self.align_hiv_nt, tree=self.tree_hiv_nt)
 
+        try:
+            import rlPyCairo as test
+            self.extended_formats: bool = True
+            del test
+        except ImportError:
+            self.extended_formats: bool = False
+
     def test_mutation_plot_inits_correctly(self):
         """ MutationPlot should initialize correctly """
 
@@ -246,9 +256,101 @@ class MutationPlotTests(unittest.TestCase):
     def test_mutation_plot_creates_valid_plot_nt(self):
         """ MutationPlot should create a valid plot """
 
-        self.mutation_plot_nt.draw('mutation/Tests/Mutation/mutation_plot_nt.svg')
-        self.assertEqual(file_hash(file_name='mutation/Tests/Mutation/mutation_plot_nt.svg'), 'a40951743cdfdd5d3b7c4d15431400fd')
-        os.remove("mutation/Tests/Mutation/mutation_plot_nt.svg")
+        hashes: dict = {
+            'mutation/Tests/Mutation/zz_nt.bmp': '4c560514d75c3046b55c1adefa81d91f',
+            'mutation/Tests/Mutation/zz_nt.eps': '51bc03aa993d918158cd4520d4e0986a',
+            'mutation/Tests/Mutation/zz_nt.gif': '081b8620d15c20ac4a9360bdd3e6b654',
+            'mutation/Tests/Mutation/zz_nt.jpg': 'b8753366700dc98a2961b9d913fcb661',
+            'mutation/Tests/Mutation/zz_nt.pdf': '993d78e8fcdab23345b3584f2d99c848',
+            'mutation/Tests/Mutation/zz_nt.png': '9af09fdc2e6ebd3b775eda952081faab',
+            'mutation/Tests/Mutation/zz_nt.ps': '51bc03aa993d918158cd4520d4e0986a',
+            'mutation/Tests/Mutation/zz_nt.svg': 'a40951743cdfdd5d3b7c4d15431400fd',
+            'mutation/Tests/Mutation/zz_nt.tif': 'c4856f168499d6d06d09712b6738aead',
+            'mutation/Tests/Mutation/zz_nt.tiff': 'c4856f168499d6d06d09712b6738aead',
+            'mutation/Tests/Mutation/zz_nt_title-False_apobec-False_gtoa-False_stop-False.svg': 'a40951743cdfdd5d3b7c4d15431400fd',
+            'mutation/Tests/Mutation/zz_nt_title-False_apobec-False_gtoa-False_stop-True.svg': '73ce6b6b7ff466c8612bc267983cee93',
+            'mutation/Tests/Mutation/zz_nt_title-False_apobec-False_gtoa-True_stop-False.svg': 'f573fbeaa87adba49f718caf64509782',
+            'mutation/Tests/Mutation/zz_nt_title-False_apobec-False_gtoa-True_stop-True.svg': '7eaf176996901444fd6e28c6abf942a6',
+            'mutation/Tests/Mutation/zz_nt_title-False_apobec-True_gtoa-False_stop-False.svg': 'f56be954ce6a24f54dd38f3eac9d70e9',
+            'mutation/Tests/Mutation/zz_nt_title-False_apobec-True_gtoa-False_stop-True.svg': 'c5e747947afaab60d6d8b9d3206ab1cb',
+            'mutation/Tests/Mutation/zz_nt_title-False_apobec-True_gtoa-True_stop-False.svg': 'f8b0b9c2c39306ab4b07d6856094d1ea',
+            'mutation/Tests/Mutation/zz_nt_title-False_apobec-True_gtoa-True_stop-True.svg': 'b714cfc0aa70b40c19d5b6bbc533372c',
+            'mutation/Tests/Mutation/zz_nt_title-True_apobec-False_gtoa-False_stop-False.svg': 'fc498f41c78c3faf3b63d96d93fdbf5e',
+            'mutation/Tests/Mutation/zz_nt_title-True_apobec-False_gtoa-False_stop-True.svg': '39cb41c9e41e5be9497d9c0edc86f54b',
+            'mutation/Tests/Mutation/zz_nt_title-True_apobec-False_gtoa-True_stop-False.svg': '82eeb0397410605523e66a12b68747ca',
+            'mutation/Tests/Mutation/zz_nt_title-True_apobec-False_gtoa-True_stop-True.svg': 'ed5dc4da1724a5a829d1618be3e7c28f',
+            'mutation/Tests/Mutation/zz_nt_title-True_apobec-True_gtoa-False_stop-False.svg': 'f8fe33991e6641ed3af9473235901f53',
+            'mutation/Tests/Mutation/zz_nt_title-True_apobec-True_gtoa-False_stop-True.svg': 'f4124815a3f8ff5568443444621ea7ed',
+            'mutation/Tests/Mutation/zz_nt_title-True_apobec-True_gtoa-True_stop-False.svg': 'ed5f8706b458daa3231e3d66c088c1c1',
+            'mutation/Tests/Mutation/zz_nt_title-True_apobec-True_gtoa-True_stop-True.svg': '05ec74319594752382635eb819c61e1a'
+        }
+
+        # PDFs have a creation date in them, so can't be checked by hash as they're different each time
+        formats = ["PS", "EPS", "SVG", "JPG", "BMP", "GIF", "PNG", "TIFF", "TIF"] # , "PDF"
+
+        for format in formats:
+            if not self.extended_formats and format in ["JPG", "BMP", "GIF", "PNG", "TIFF", "TIF"]:
+                continue
+
+            with self.subTest(format=format):
+                file_name: str = f"mutation/Tests/Mutation/zz_nt.{format.lower()}"
+                self.mutation_plot_nt.draw(file_name, output_format=format)
+                self.assertEqual(file_hash(file_name=file_name), hashes[file_name])
+                os.remove(file_name)
+
+        titles: list = [None, "A Title"]
+        apobecs: list = [True, False]
+        g_to_as: list = [True, False]
+        stop_codonss: list = [True, False]
+
+        for title, apobec, g_to_a, stop_codons in product(titles, apobecs, g_to_as, stop_codonss):
+            with self.subTest(title=title, apobec=apobec, g_to_a=g_to_a, stop_codons=stop_codons):
+                file_name: str = f"mutation/Tests/Mutation/zz_nt_title-{bool(title)}_apobec-{apobec}_gtoa-{g_to_a}_stop-{stop_codons}.svg"
+                self.mutation_plot_nt.draw(file_name, title=title, apobec=apobec, g_to_a=g_to_a, stop_codons=stop_codons)
+                self.assertEqual(file_hash(file_name=file_name), hashes[file_name])
+                os.remove(file_name)
+
+    def test_mutation_plot_creates_valid_plot_aa(self):
+        """ MutationPlot should create a valid plot """
+
+        hashes: dict = {
+            'mutation/Tests/Mutation/zz_aa.bmp': '6202b8038419e4845f56c6f50dfc6d12',
+            'mutation/Tests/Mutation/zz_aa.eps': 'a626c585761825a7c42480fb0f89d7cc',
+            'mutation/Tests/Mutation/zz_aa.gif': '8890a7db5610b0afc3120dd71de8c9e0',
+            'mutation/Tests/Mutation/zz_aa.jpg': '63163e9f52a96a61eaaffaa1323e5bff',
+            'mutation/Tests/Mutation/zz_aa.png': '14e5519b9d1ed682edadb5d915950b7a',
+            'mutation/Tests/Mutation/zz_aa.ps': 'a626c585761825a7c42480fb0f89d7cc',
+            'mutation/Tests/Mutation/zz_aa.svg': 'c3ecb8cd9b76f70631d9ac0e78318ac3',
+            'mutation/Tests/Mutation/zz_aa.tif': '5c94dad4c1608207aa627b1af40078ab',
+            'mutation/Tests/Mutation/zz_aa.tiff': '5c94dad4c1608207aa627b1af40078ab',
+            'mutation/Tests/Mutation/zz_aa_title-False_glycosylation-False.svg': 'c3ecb8cd9b76f70631d9ac0e78318ac3',
+            'mutation/Tests/Mutation/zz_aa_title-False_glycosylation-True.svg': '6baa39bb672beef0a579180a21fddc7f',
+            'mutation/Tests/Mutation/zz_aa_title-True_glycosylation-False.svg': '223bbaff099f3267481c1b5076f0cf63',
+            'mutation/Tests/Mutation/zz_aa_title-True_glycosylation-True.svg': 'ee6576d9f7ef8cdc908507ae22df4d8a'
+        }
+
+        # PDFs have a creation date in them, so can't be checked by hash as they're different each time
+        formats = ["PS", "EPS", "SVG", "JPG", "BMP", "GIF", "PNG", "TIFF", "TIF"] # , "PDF"
+
+        for format in formats:
+            if not self.extended_formats and format in ["JPG", "BMP", "GIF", "PNG", "TIFF", "TIF"]:
+                continue
+
+            with self.subTest(format=format):
+                file_name: str = f"mutation/Tests/Mutation/zz_aa.{format.lower()}"
+                self.mutation_plot_aa.draw(file_name, output_format=format)
+                self.assertEqual(file_hash(file_name=file_name), hashes[file_name])
+                os.remove(file_name)
+
+        titles: list = [None, "A Title"]
+        glycosylations: list = [True, False]
+
+        for title, glycosylation in product(titles, glycosylations):
+            with self.subTest(title=title, glycosylation=glycosylation):
+                file_name: str = f"mutation/Tests/Mutation/zz_aa_title-{bool(title)}_glycosylation-{glycosylation}.svg"
+                self.mutation_plot_aa.draw(file_name, title=title, glycosylation=glycosylation)
+                self.assertEqual(file_hash(file_name=file_name), hashes[file_name])
+                os.remove(file_name)
 
 if __name__ == '__main__':
     unittest.main()
